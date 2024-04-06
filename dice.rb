ad25dd8678bf41e88a0cda1847e3d3d65104e29a
Dtraub21 <164813811+Dtraub21@users.noreply.github.com>
2024-04-06 22:08:11 +0000

    views

--- dice.rb
+++ dice.rb
@@ -3,9 +3,19 @@
 require "sinatra"
 require "sinatra"
 
-get("/") do
-  "Hello World"
-end
+
+  get("/") do
+    "
+    <h1>Dice Roll</h1>
+    
+    <ul>
+      <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+      <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+      <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+      <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    </ul>
+    "
+  end
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
@@ -71,3 +81,8 @@ require "binding_of_caller"
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  erb(:elephant)
+end
+
